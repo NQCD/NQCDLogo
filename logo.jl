ba73dcd2776802263a2f624721408c9f78e429b9
James Gardner <james.gardner1421@gmail.com>
2022-02-01 09:56:53 +0000

    Add logo with text and rearrange

--- logo.jl
+++ logo.jl
@@ -8,15 +8,6 @@ function gradient(p1, p2)
     return diff[2] / diff[1]
 end
 
-Drawing(2px+linewidth, 2px+linewidth, :png, "logo.png")
-background(1, 1, 1, 0)
-origin()
-
-setline(linewidth)
-rounding = 0.4
-squircle(Point(0, 0), px, px, rt=rounding, :clip)
-background(Luxor.julia_blue...)
-
 topleft = Point(-px, -px)
 topright = Point(px, -px)
 term1 = Point(-px/4+px/30, px/3)
@@ -45,38 +36,58 @@ internal = px/15
 long = px/3
 short = px/30*8
 
-move(topleft)
-curve(A, B, term1)
-curve(mid1, apex + Point(-px/15, 0), apex)
-curve(apex + Point(-short, 0), topanchor1 + Point(internal, long), topanchor1)
-line(topleft)
-sethue(Luxor.julia_red)
-fillpreserve()
-sethue(linecolor)
-strokepath()
+function drawlogo()
+    setline(linewidth)
+    rounding = 0.4
+    squircle(Point(0, 0), px, px, rt=rounding, :clip)
+    background(Luxor.julia_blue...)
 
-move(topright)
-curve(D, C, term2)
-curve(mid2, apex + Point(px/15, 0), apex)
-curve(apex + Point(short, 0), topanchor2 + Point(-internal, long), topanchor2)
-line(topright)
-sethue(Luxor.julia_purple)
-fillpreserve()
-sethue(linecolor)
-strokepath()
+    move(topleft)
+    curve(A, B, term1)
+    curve(mid1, apex + Point(-px/15, 0), apex)
+    curve(apex + Point(-short, 0), topanchor1 + Point(internal, long), topanchor1)
+    line(topleft)
+    sethue(Luxor.julia_red)
+    fillpreserve()
+    sethue(linecolor)
+    strokepath()
 
-move(topanchor1)
-line(topanchor2)
-curve(topanchor2 + Point(-internal, long), apex + Point(short, 0), apex)
-curve(apex + Point(-short, 0), topanchor1 + Point(internal, long), topanchor1)
-sethue(Luxor.julia_green)
-fillpreserve()
-sethue(linecolor)
-strokepath()
+    move(topright)
+    curve(D, C, term2)
+    curve(mid2, apex + Point(px/15, 0), apex)
+    curve(apex + Point(short, 0), topanchor2 + Point(-internal, long), topanchor2)
+    line(topright)
+    sethue(Luxor.julia_purple)
+    fillpreserve()
+    sethue(linecolor)
+    strokepath()
 
-clipreset()
-sethue(linecolor)
-squircle(Point(0, 0), px, px, rt=rounding, :stroke)
+    move(topanchor1)
+    line(topanchor2)
+    curve(topanchor2 + Point(-internal, long), apex + Point(short, 0), apex)
+    curve(apex + Point(-short, 0), topanchor1 + Point(internal, long), topanchor1)
+    sethue(Luxor.julia_green)
+    fillpreserve()
+    sethue(linecolor)
+    strokepath()
+
+    clipreset()
+    sethue(linecolor)
+    squircle(Point(0, 0), px, px, rt=rounding, :stroke)
+end
+
+function addtext()
+    fontface("Fira Sans")
+    fontsize(linewidth*12)
+    text("NQCD", Point(-px/4, 2px/3), valign=:middle, halign=:left)
+end
+
+Drawing(2px+linewidth, 2px+linewidth, :png, "images/logo_with_text.png")
+background(1, 1, 1, 0)
+origin()
+
+drawlogo()
+addtext()
 
 finish()
 preview()
